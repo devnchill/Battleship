cd84933d478fcfebf1b489f584591f203a834814
0xDev <169875752+devnchill@users.noreply.github.com>
2025-03-28 21:31:33 +0530

    chore: Initial Setup and Implement Ship And it's Child Class

--- README.md
+++
@@ -1,2 +0,0 @@
-# TSTemplate
-A template repo for jump start to project without the need to setup environment everytime.
---
+++ src/Components/Ship.ts
@@ -0,0 +1,47 @@
+class Ship {
+  //instance variables (unique for each ship object)
+  private hitCount: number;
+  private readonly length: number;
+
+  constructor(length: number) {
+    this.length = length;
+    this.hitCount = 0;
+  }
+
+  hit(): void {
+    this.hitCount++;
+  }
+
+  get health(): number {
+    return this.length - this.hitCount;
+  }
+
+  isSunk(): boolean {
+    return this.hitCount >= this.length;
+  }
+}
+class Carrier extends Ship {
+  constructor() {
+    super(5);
+  }
+}
+class Battleship extends Ship {
+  constructor() {
+    super(4);
+  }
+}
+class Cruiser extends Ship {
+  constructor() {
+    super(3);
+  }
+}
+class Submarine extends Ship {
+  constructor() {
+    super(3);
+  }
+}
+class Destroyer extends Ship {
+  constructor() {
+    super(2);
+  }
+}

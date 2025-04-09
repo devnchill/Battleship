c24d318c30d9987fde99b79a0dd670a5e7bfcba7
0xDev <169875752+devnchill@users.noreply.github.com>
2025-04-09 22:27:36 +0530

    chore: renamed getter to get board and corrected typo

--- __tests__/GameBoard.test.ts
+++ __tests__/GameBoard.test.ts
@@ -6,7 +6,7 @@ import { Direction } from "../src/Types/ship.types";
 describe("Test for GameBoard", () => {
   test("make sure it creates a 2D 10x10 array with appropriate properties", () => {
     const gameBoard = new GameBoard();
-    const board = gameBoard.getBoard;
+    const board = gameBoard.board;
 
     expect(board.length).toBe(10);
 
@@ -35,22 +35,22 @@ describe("Test for GameBoard", () => {
   test("test for placeShip", () => {
     const gameBoard = new GameBoard();
     gameBoard.placeShip(new Ship(3), [0, 0]);
-    expect(gameBoard.getBoard[0][0].hasShip).toBeTruthy();
-    expect(gameBoard.getBoard[0][1].hasShip).toBeTruthy();
-    expect(gameBoard.getBoard[0][2].hasShip).toBeTruthy();
-    expect(gameBoard.getBoard[1][0].hasShip).toBeFalsy();
+    expect(gameBoard.board[0][0].hasShip).toBeTruthy();
+    expect(gameBoard.board[0][1].hasShip).toBeTruthy();
+    expect(gameBoard.board[0][2].hasShip).toBeTruthy();
+    expect(gameBoard.board[1][0].hasShip).toBeFalsy();
     gameBoard.placeShip(new Ship(3, Direction.VERTICAL), [0, 0]);
-    expect(gameBoard.getBoard[1][0].hasShip).toBeTruthy();
-    expect(gameBoard.getBoard[2][0].hasShip).toBeTruthy();
-    expect(gameBoard.getBoard[3][0].hasShip).toBeFalsy();
+    expect(gameBoard.board[1][0].hasShip).toBeTruthy();
+    expect(gameBoard.board[2][0].hasShip).toBeTruthy();
+    expect(gameBoard.board[3][0].hasShip).toBeFalsy();
   });
 
-  test("test for recieveAttack", () => {
+  test("test for receiveAttack", () => {
     const gameBoard = new GameBoard();
     gameBoard.placeShip(new Ship(5), [0, 0]);
-    gameBoard.recieveAttack([0, 0]);
-    expect(gameBoard.getBoard[0][0].ship?.health).toBe(4);
-    expect(gameBoard.getBoard[0][0].state).toBe(CellState.HIT);
-    expect(gameBoard.getBoard[0][9].state).toBe(CellState.UNTOUCHED);
+    gameBoard.receiveAttack([0, 0]);
+    expect(gameBoard.board[0][0].ship?.health).toBe(4);
+    expect(gameBoard.board[0][0].state).toBe(CellState.HIT);
+    expect(gameBoard.board[0][9].state).toBe(CellState.UNTOUCHED);
   });
 });

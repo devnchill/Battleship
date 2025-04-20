77e2dcabdfbdf42e4d289ea81369e4220ce6c2c1
0xDev <169875752+devnchill@users.noreply.github.com>
2025-04-21 00:25:20 +0530

    feat: startup dialog setup

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import "./css/styles.css";
-import { GameController } from "./Core/GameController";
-
-const controller = new GameController();
-const humanPlayer = controller.human.gameBoard.board;
-const aiPlayer = controller.ai.gameBoard.board;
-console.log("Human's Board", humanPlayer);
-console.log("Ai's Board", aiPlayer);
-controller.makeMove();
+// import { GameController } from "./Core/GameController";
+//
+// const controller = new GameController();
+// const humanPlayer = controller.human.gameBoard.board;
+// const aiPlayer = controller.ai.gameBoard.board;
+// console.log("Human's Board", humanPlayer);
+// console.log("Ai's Board", aiPlayer);
+// controller.makeMove();
